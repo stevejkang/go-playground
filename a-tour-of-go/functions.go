59d59da28779c08b2f1a1a57cc515ed1876722dc
Steve Kang <iam@juneyoung.io>
2022-01-04 20:20:21 +0900

    Add multiple return function example

--- a-tour-of-go/functions.go
+++ a-tour-of-go/functions.go
@@ -6,6 +6,13 @@ func add(x, y int) int { // same as `func add(x int, y int) int`
 	return x + y
 }
 
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
 func main() {
 	fmt.Println(add(42, 13))
+
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
 }
